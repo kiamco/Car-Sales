d7873ae964b55da05939a375e6824f34701e52d8
kiamco <kiamcoo@gmail.com>
2019-09-24 22:08:06 -0700

    it begins

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
 
 import 'bulma/css/bulma.css';
 import './styles.scss';
+import { create } from 'domain';
 
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+const store = createStore()
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, rootElement);
