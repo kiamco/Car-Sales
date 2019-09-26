01f181bb4c8d83c00f9b61cea830ecd391bd7cb2
kiamco <kiamcoo@gmail.com>
2019-09-25 22:22:53 -0700

    mvp completed

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
+import { featureReducer } from './reducers/index';
 
 
 import 'bulma/css/bulma.css';
 import './styles.scss';
-import { create } from 'domain';
 
 const rootElement = document.getElementById('root');
-const store = createStore()
+const store = createStore(featureReducer);
 
 ReactDOM.render(
     <Provider store={store}>
